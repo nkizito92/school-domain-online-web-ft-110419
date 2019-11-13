c45304260a9f8ecf8a466d6ba9f4064d12397c70
Kizito Njoku <nkizito57@gmail.com>
2019-11-13 03:25:39 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -8,7 +8,7 @@ class School
   end
   def initialize(school_name)
     @school_name = school_name
-    @roster = Hash.new{ |h, k| h[k] = [] }    
+    @roster = Hash.new{ |student, grade| student[grade] = [] }    
   end 
   def add_student(student_name, grade) 
      @roster[grade] << student_name 
