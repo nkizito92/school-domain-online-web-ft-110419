25863de457ed0dd0f22a77bb9e731553eee8ba0d
Kizito Njoku <nkizito57@gmail.com>
2019-11-13 03:23:01 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,28 @@
 # code here!
+class School 
+  attr_accessor :roster 
+  attr_reader :model, :roster, :add_student
+  
+  def initialize(roster)
+    @roster = []
+  end
+  def initialize(school_name)
+    @school_name = school_name
+    @roster = Hash.new{ |h, k| h[k] = [] }    
+  end 
+  def add_student(student_name, grade) 
+     @roster[grade] << student_name 
+  end 
+  
+  def grade(grade)
+    @roster[grade]
+  end 
+  def sort 
+    student = {} 
+    roster.each do |name, grade|
+      student[name] = grade.sort
+    end
+    student
+  end 
+end 
+school = School.new("Bayside High School")
